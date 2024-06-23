8e449fd675b948535a49a438e2db05f7a9eb6cda
joshmarek22 <josh.marek22@gmail.com>
2024-06-22 21:36:12 -0400

    added button ui to pick ops

--- main.py
+++ main.py
@@ -16,24 +16,58 @@ operator = json.load(open('operators.json'))
 attackersNum = len(operator["operators"]["attackers"])
 defendersNum = len(operator["operators"]["defenders"])
 
+def getAttacker():
+    randomNum = random.randint(0,attackersNum - 1)
+    randomAttacker = operator["operators"]["attackers"][randomNum]
+    return randomAttacker
+
+def getDefender():
+    randomNum = random.randint(0,defendersNum - 1)
+    randomDefender = operator["operators"]["defenders"][randomNum]
+    return randomDefender
+
+
+class myView(discord.ui.View):
+    @discord.ui.button(label="Attacker", style=discord.ButtonStyle.blurple)
+    async def attacker(self,  interaction: discord.interactions, button: discord.ui.Button):
+        attacker = getAttacker()
+        print(f"{interaction.user.global_name} got: {attacker}")
+        await interaction.response.send_message(f"{interaction.user.global_name} got: {attacker}", delete_after=6)
+
+    @discord.ui.button(label="Defender", style=discord.ButtonStyle.danger)
+    async def defender(self,  interaction: discord.interactions, button: discord.ui.Button):
+        defender = getDefender()
+        print(f"{interaction.user.global_name} got: {defender}")
+        await interaction.response.send_message(f"{interaction.user.global_name} got: {defender}" , delete_after=6)
+
 
 @bot.event
 async def on_ready(): 
     print("Online")
 
-@bot.command(name="attacker")
-async def attacker(ctx):
-    randomNum = random.randint(0,attackersNum - 1)
-    randomAttacker = operator["operators"]["attackers"][randomNum]
-    print(f"{ctx.author.global_name} got: {randomAttacker}")
-    await ctx.send(f"{ctx.author.global_name} got: {randomAttacker}", delete_after=30)
 
+@bot.command()
+async def rndOps(ctx):
+    view = myView()
+    await ctx.send(view=view)
 
-@bot.command(name="defender")
-async def defender(ctx):
-    randomNum = random.randint(0,defendersNum - 1)
-    randomDefender = operator["operators"]["defenders"][randomNum]
-    print(f"{ctx.author.global_name} got: {randomDefender}")
-    await ctx.send(f"{ctx.author.global_name} got: {randomDefender}", delete_after=30)
 
 bot.run(TOKEN)
+
+
+
+
+# @bot.command(name="attacker")
+# async def attacker(ctx):
+#     randomNum = random.randint(0,attackersNum - 1)
+#     randomAttacker = operator["operators"]["attackers"][randomNum]
+#     print(f"{ctx.author.global_name} got: {randomAttacker}")
+#     await ctx.send(f"{ctx.author.global_name} got: {randomAttacker}", delete_after=30)
+
+
+# @bot.command(name="defender")
+# async def defender(ctx):
+#     randomNum = random.randint(0,defendersNum - 1)
+#     randomDefender = operator["operators"]["defenders"][randomNum]
+#     print(f"{ctx.author.global_name} got: {randomDefender}")
+#     await ctx.send(f"{ctx.author.global_name} got: {randomDefender}", delete_after=30)
